2df3c22559009c9bbc64d2b248aec3579cf72c61
Hilmi <hilmi@gementar.com>
2025-05-27 11:36:48 +0800

    prog

--- src/app/portal/(claim)/admin/user-settings/page.tsx
+++ src/app/portal/(claim)/admin/user-settings/page.tsx
@@ -1,12 +1,13 @@
   import React from 'react'
   import { checkPagePermission } from '~/lib/permissions/check-page-permission';
   import { PermissionList } from '@prisma/client';
+import { UserTable } from './components/user-table';
 
 const page = async () => {
   await checkPagePermission(PermissionList.PAGE_PERMISSION_USER_SETTINGS);
   
   return (
-    <div>page</div>
+    <div><UserTable /></div>
   )
 }
 
